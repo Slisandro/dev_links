081e315e17f304e3ddea73d713cf16821c1ddec3
Lisandro <salvareschilisandro0@gmail.com>
2023-10-22 18:10:16 -0300

    add missing properties

--- src/components/edit-links-component.tsx
+++ src/components/edit-links-component.tsx
@@ -7,7 +7,7 @@ function EditLinksComponent() {
         <h2>Customize your links</h2>
         <h4>Add/edit/remove links below and then share all your profiles with the world!</h4>
       </div>
-      <ButtonComponent label={"+ Add new link"} type="outline" />
+      <ButtonComponent label={"+ Add new link"} type="outline" disabled={false} />
       <div className="links">
         <svg xmlns="http://www.w3.org/2000/svg" width="200" height="120" viewBox="0 0 250 161" fill="none">
           <path opacity="0.3" d="M48.6936 15.4213C23.3786 25.2238 4.59362 50.0679 0.857884 80.1285C-2.26282 105.459 5.19347 133.446 49.0884 141.419C134.494 156.939 222.534 158.754 242.952 116.894C263.369 75.0336 235.427 8.00293 192.079 3.36363C157.683 -0.326546 98.1465 -3.7206 48.6936 15.4213Z" fill="white" />
@@ -52,7 +52,7 @@ function EditLinksComponent() {
         <p>Use the “Add new link” button to get started. Once you have more than one link, you can reorder and edit them. We’re here to help you share your profiles with everyone!</p>
       </div>
       <div className="actions">
-        <ButtonComponent label="Save" type="outline" />
+        <ButtonComponent label="Save" type="outline" disabled={true} />
       </div>
     </div>
   )
