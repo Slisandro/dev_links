3304359c191721cc0704ec91ff37d46dd614a10f
Lisandro <salvareschilisandro0@gmail.com>
2023-10-24 13:46:36 -0300

    Commit message

--- src/redux/reducers/index.tsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import userProfileReducers from "./user-profile-reducers";
-
-const rootReducer = combineReducers({
-    userProfileReducers
-});
-
-export default rootReducer;
