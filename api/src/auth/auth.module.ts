64786208e510077567cdc58dc9b7a0784d66a5d6
Lisandro <salvareschilisandro0@gmail.com>
2023-11-02 11:38:32 -0300

    update auth module

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { JwtStrategy } from '../strategies/jwt.strategy';
 import { ProfileModule } from 'src/profile/profile.module';
+import { LinkModule } from 'src/links/links.module';
 
 @Module({
-    imports: [UsersModule, ProfileModule],
+    imports: [UsersModule, ProfileModule, LinkModule],
     providers: [AuthService, JwtStrategy],
     controllers: [AuthController]
 })
