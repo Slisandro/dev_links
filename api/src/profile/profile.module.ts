e64a08f52e1a90e2b729f781937c11753f0ab695
Lisandro <salvareschilisandro0@gmail.com>
2023-11-02 19:08:06 -0300

    update profile module

--- api/src/profile/profile.module.ts
+++ api/src/profile/profile.module.ts
@@ -4,10 +4,12 @@ import { Profile } from './profile.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProfileController } from './profile.controller';
 import { Storage } from '@google-cloud/storage';
+import { LinkService } from 'src/links/links.service';
+import { Link } from 'src/links/links.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Profile])],
-  providers: [ProfileService, Storage],
+  imports: [TypeOrmModule.forFeature([Profile, Link])],
+  providers: [ProfileService, LinkService, Storage],
   controllers: [ProfileController],
   exports: [ProfileService]
 })
