0e41782c95cc5134ff7eecc7bf442b72b9f52e43
Lisandro <salvareschilisandro0@gmail.com>
2023-11-04 22:15:59 -0300

    Add NavLink and Copy Link functionality to PreviewLayout

--- app/src/layouts/preview-layout.tsx
+++ app/src/layouts/preview-layout.tsx
@@ -1,10 +1,40 @@
+import { NavLink } from 'react-router-dom'
 import PreviewPhoneProfileComponent from '../components/preview-phone-profile-component'
+import LinkIcon from '../icons/link-icon'
 
 function PreviewLayout() {
+    const handleCopyLink = () => {
+        const link = document.getElementById('link');
+        if (link) { 
+            // @ts-ignore
+            link.select();
+            // @ts-ignore
+            navigator.clipboard.writeText(link.value);
+        }
+    }
     return (
-        <section>
+        <main className="container-preview-layout">
+            <div className="header">
+                <NavLink to="/home">
+                    <button>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
+                        </svg>
+                        Home
+                    </button>
+                </NavLink>
+            </div>
+            <div className="actions">
+                <div className="textfield">
+                    <input id="link" type="text" value="https://www.linkedin.com/in/username" disabled />
+                </div>
+                <button onClick={handleCopyLink}>
+                    <LinkIcon color="currentColor" />
+                    Copy Link
+                </button>
+            </div>
             <PreviewPhoneProfileComponent />
-        </section>
+        </main>
     )
 }
 
