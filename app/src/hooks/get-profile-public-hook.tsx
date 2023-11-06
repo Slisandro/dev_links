276c32e70ba7715f40da5d76dc179dba39b58204
Lisandro <salvareschilisandro0@gmail.com>
2023-11-06 11:48:33 -0300

    Add useProfilePublicHook to fetch public profile data

---
+++ app/src/hooks/get-profile-public-hook.tsx
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import React from 'react';
+import { Link } from '../redux/reducers/links-reducers';
+
+function useProfilePublicHook(id: string) {
+    const [profile, setProfile] = React.useState
+        <{
+            name: string,
+            lastname: string,
+            username: string,
+            image: string,
+            links: Link[]
+        }>({
+            name: "",
+            lastname: "",
+            username: "",
+            image: "",
+            links: [
+                { type: "", url: "" },
+                { type: "", url: "" },
+                { type: "", url: "" },
+                { type: "", url: "" },
+                { type: "", url: "" }
+            ]
+        });
+
+    React.useEffect(() => {
+        if (id) {
+            axios.get(`http://localhost:3000/profile/${id}`)
+                .then(r => setProfile(r.data))
+                .catch(() => null)
+        }
+    }, [id]);
+
+    return {
+        profile
+    }
+}
+
+export default useProfilePublicHook
