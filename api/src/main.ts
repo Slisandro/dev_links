b73f97e272f7298123bae6b7150f2662abcd7625
Slisandro <salvareschilisandro0@gmail.com>
2025-02-15 13:17:31 -0300

    add dotenv

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173", "https://devlinks-sl.vercel.app"],
   })
   // @ts-expect-error
   await app.listen(parseInt(process.env.PORT, '0.0.0.0') || 3000);
 }
+
 bootstrap();
