8341ce10ffaa79660965ec9ee884e5d00da997bc
Lisandro <salvareschilisandro0@gmail.com>
2026-06-13 15:41:38 -0300

    Update main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:5173", "https://devlinks-sl.vercel.app/"],
-  })
+    origin: true, // Permite cualquier origen
+    credentials: true,
+  });
   // @ts-expect-error
   await app.listen(parseInt(process.env.PORT, '0.0.0.0') || 3000);
 }
