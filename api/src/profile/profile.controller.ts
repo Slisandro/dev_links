9e6353c960f88fa7b35c1eb09f45954c10fed695
Lisandro <salvareschilisandro0@gmail.com>
2026-06-13 16:15:58 -0300

    Update profile.controller.ts

--- api/src/profile/profile.controller.ts
+++ api/src/profile/profile.controller.ts
@@ -1,5 +1,4 @@
-import { initializeApp } from 'firebase/app';
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { Storage } from '@google-cloud/storage';
 import { BadRequestException, Body, Controller, Get, Param, Post, Put, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { LinkService } from 'src/links/links.service';
@@ -9,21 +8,15 @@ import { Profile } from './profile.entity';
 @Controller('profile')
 export class ProfileController {
 
-    private storage;
-
     constructor(
+        private storage: Storage,
         private userProfileService: ProfileService,
         private linkService: LinkService
     ) {
-        const firebaseConfig = {
-            apiKey: process.env.FIREBASE_API_KEY,
-            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        };
-        
-        const app = initializeApp(firebaseConfig);
-        this.storage = getStorage(app);
+        this.storage = new Storage({
+            projectId: 'roomies-2096e',
+            keyFilename: './src/profile/credentials.storage.json'
+        })
     }
 
     @Get(':id')
@@ -60,17 +53,21 @@ export class ProfileController {
     @Post('upload-image')
     @UseInterceptors(FileInterceptor('image'))
     async updateImage(@UploadedFile() image) {
+        // ad method remove image in the future
         const randomName = Array(32).fill(null).map(() => Math.round(Math.random() * 16).toString(16)).join('');
         const filename = `${randomName}${image.originalname}`;
-        
+        const bucket = this.storage.bucket('roomies-2096e.appspot.com');
+        const file = bucket.file(filename);
+
         try {
-            const storageRef = ref(this.storage, filename);
-            await uploadBytes(storageRef, image.buffer, {
-                contentType: image.mimetype,
+            await file.save(image.buffer, {
+                metadata: {
+                    contentType: image.mimetype,
+                },
             });
-            
-            const downloadUrl = await getDownloadURL(storageRef);
-            return downloadUrl;
+
+            await file.makePublic();
+            return `https://firebasestorage.googleapis.com/v0/b/packflowing-3ee65.firebasestorage.app/o/${filename}?alt=media`;
         } catch (error) {
             console.error(error);
             throw new BadRequestException('Error uploading image');
